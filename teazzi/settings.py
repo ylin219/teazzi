c70111450a94c38a752f5330ab227fa6f69bc4b5
ylin219 <42956667+ylin219@users.noreply.github.com>
2022-03-23 21:40:58 -0400

    Update settings.py

--- teazzi/settings.py
+++ teazzi/settings.py
@@ -120,11 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+if not DEBUG:
+    STATIC_ROOT=''
+
 STATICFILES_DIRS = [
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = '/static/'
+# STATIC_ROOT = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
